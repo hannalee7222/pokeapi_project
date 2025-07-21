63a42056352edeb8f37d56ce23bf8b57f675f737
hannalee7222 <hannah.lee7222@gmail.com>
2025-07-21 23:37:43 +0900

    📍리액트 18일차 과제- 포켓몬 도감 만들기

---
+++ src/RTK/slice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchMultiplePokemonById } from './thunk';
+import { act } from 'react';
+
+export const pokemonSlice = createSlice({
+  name: 'pokemon',
+  initialState: {
+    data: [],
+    loading: true,
+  },
+  reducers: {}, //동기적 상태 변경
+  extraReducers: (builder) => {
+    //비동기적 상태 변경
+    builder
+      .addCase(fetchMultiplePokemonById.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchMultiplePokemonById.rejected, (state) => {
+        state.loading = false;
+      })
+      .addCase(fetchMultiplePokemonById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      });
+  },
+});
+
+export const favoriteSlice = createSlice({
+  name: 'favorite',
+  initialState: [1, 2, 3],
+  reducers: {
+    addToFavorite(state, action) {
+      state.push(action.payload.pokemonId);
+    },
+    removeFromFavorite(state, action) {
+      const index = state.indexOf(action.payload.pokemonId);
+      if (index !== -1) state.splice(index, 1);
+    },
+  },
+});
---
+++ src/component/FlipCard.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const FlipImageContainer = styled.div`
+  width: 200px;
+  height: 200px;
+  position: relative;
+  transform-style: preserve-3d;
+  transition: 0.5s;
+  transform: ${(props) => (props.fliped ? 'rotateY(180deg)' : 'rotateY(0deg)')};
+`;
+
+const FrontImage = styled.img`
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  position: absolute;
+`;
+
+const BackImage = styled.img`
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  transform: rotateY(180deg);
+`;
+
+export default function FlipCard({ front, back }) {
+  const [fliped, setFliped] = useState(false);
+  return (
+    <>
+      <FlipImageContainer fliped={fliped ? 'flip' : ''}>
+        <FrontImage src={front} />
+        <BackImage src={back} />
+      </FlipImageContainer>
+      <button onClick={() => setFliped((prev) => !prev)}>뒤집기</button>
+    </>
+  );
+}
